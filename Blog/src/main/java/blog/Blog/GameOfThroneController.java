8fca4cee018a9c011e634e597da74ddf46ea51b5
solo-utd <solomontm11@gmail.com>
2018-11-01 14:27:04 -0500

    Initial commit

---
+++ Blog/src/main/java/blog/Blog/GameOfThroneController.java
@@ -0,0 +1,28 @@
+package blog.Blog;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class GameOfThroneController {
+
+    private List<Solider> soliderlist = new ArrayList<>(Arrays.asList(new Solider("jhon Snow", 123),new Solider("janous",321)));
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String hello(@RequestParam(defaultValue = "stranger")String name){
+        return "hello "+name+" do you pledge your life to the night watch!";
+    }
+
+    @RequestMapping(value = "/solider", method = RequestMethod.GET)
+    public List<Solider> list(){
+        return soliderlist;
+    }
+
+    @RequestMapping(value = "/new1", method = RequestMethod.POST)
+    private Solider newone(@RequestBody Solider solider){
+        soliderlist.add(solider);
+        return solider;
+    }
+}
---
+++ Blog/src/main/java/blog/Blog/Solider.java
@@ -0,0 +1,31 @@
+package blog.Blog;
+
+public class Solider {
+
+    private String name;
+    private int number;
+
+    public Solider() {
+    }
+
+    public Solider(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
